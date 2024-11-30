847bb7f9fd5d94f651b5cbfb7e26c704f2437e73
Rajesh-03 <tr98523@gmail.com>
2024-12-01 04:39:35 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during build
+  },
   /* config options here */
 };
 
